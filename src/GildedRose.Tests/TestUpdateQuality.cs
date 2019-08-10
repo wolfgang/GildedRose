43e0ed45a240317b9e4d1686719497ff77568e7b
wolfgang <wolfgang@wolfgangdeutsch.work>
2019-08-10 10:21:01 +0200

    Remve XUnit, add NUnit, write first dummy unit test

---
+++ src/GildedRose.Tests/TestUpdateQuality.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+
+namespace GildedRose.Tests {
+    public class TestUpdateQuality {
+        [Test]
+        public void ItemsAfterOnDay0() {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
